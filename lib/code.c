ed35758686a48a3f1b58bd9d2dfe72d3828fcec4
Bouboul30 <quentin.faivre30@gmail.com>
2015-12-15 10:07:28 +0100

    Ajout du makefile et de la grammaire pour faire le CParser

---
+++ lib/code.c
@@ -0,0 +1,24 @@
+//#include <stdio.h>
+
+/*int main() {
+	int x;
+	int y;
+	y = 0;
+	x = 1;
+	x = x + 2;
+	
+	while (x < 10){
+		y = y + x;
+		x = x + 1;
+	};
+	printf("Hello world !\n");
+	return 0;
+}*/
+int x;
+int z;
+void fct(){
+	int y;
+	int z;
+	y = 1;
+	y = 2;
+}
