1da8a8da60904b3e3a74b7c7e7b713c68215cdd3
Gaellic <gaellic.rannou@imerir.com>
2015-12-15 10:30:44 +0100

    compiler terminé

--- ErrorMeasurement/src/Compiler/CCompiler.java
+++ ErrorMeasurement/src/Compiler/CCompiler.java
@@ -23,11 +23,10 @@ public class CCompiler {
 	
 	void Compile()
 	{
-		/*
+		
 		try {  
             String exeName = fileName.substring(0, fileName.length() - 2);
-            //Process p = Runtime.getRuntime().exec("gcc " + fileName + " -o " + exeName, null, dir);
-            Process p = Runtime.getRuntime().exec("mkdir " + fileName, null, dir); 
+            Process p = Runtime.getRuntime().exec("gcc " + fileName + " -o " + exeName, null, dir);
             BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));  
             String line = null;  
             while ((line = in.readLine()) != null) {  
@@ -35,19 +34,8 @@ public class CCompiler {
             }  
         } catch (IOException e) {  
             e.printStackTrace();  
-        } */
-		/*
-		try {
-		    // Execute a command with an argument that contains a space
-		    String[] commands = new String[]{"grep", "hello world", "/tmp/f.txt"};
-		     
-		    commands = new String[]{"grep", "hello world", 
-		            "/home/gaellic/Documents/f.txt"};
-		             
-		    Process child = Runtime.getRuntime().exec(commands);
-		     
-		} catch (IOException e) {
-		}*/
+        } 
+		
 		System.out.println("Compile!");
 
 		
@@ -57,18 +45,12 @@ public class CCompiler {
 		// TODO Auto-generated method stub
 		
 		String fileName="helloWorld.c";
-		File dir = new File("/home/gaellic/workspace/M2Projet/ErrorMeasurement/bin/Compiler");
+		File dir = new File("programmeC");
+		///home/gaellic/workspace/M2Projet/ErrorMeasurement/
 		CCompiler test = new CCompiler(fileName,dir);
 		test.Compile();
 		
-		Runtime runtime = Runtime.getRuntime();
-		String[] arg = { "/bin/sh", "-c", "ls / >fichier.txt" };
-		try {
-			final Process process = runtime.exec(arg);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
 		
 		System.out.print("sa marche!");
 		
