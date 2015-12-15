fb26a18e7337e25afe9c1df91ea893a7a596ae04
Gaellic <gaellic.rannou@imerir.com>
2015-12-15 10:09:54 +0100

    compiler

--- ErrorMeasurement/src/Compiler/CCompiler.java
+++ ErrorMeasurement/src/Compiler/CCompiler.java
@@ -23,9 +23,11 @@ public class CCompiler {
 	
 	void Compile()
 	{
+		/*
 		try {  
             String exeName = fileName.substring(0, fileName.length() - 2);
-            Process p = Runtime.getRuntime().exec("cmd /C gcc " + fileName + " -o " + exeName, null, dir);   
+            //Process p = Runtime.getRuntime().exec("gcc " + fileName + " -o " + exeName, null, dir);
+            Process p = Runtime.getRuntime().exec("mkdir " + fileName, null, dir); 
             BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));  
             String line = null;  
             while ((line = in.readLine()) != null) {  
@@ -33,16 +35,42 @@ public class CCompiler {
             }  
         } catch (IOException e) {  
             e.printStackTrace();  
-        } 
+        } */
+		/*
+		try {
+		    // Execute a command with an argument that contains a space
+		    String[] commands = new String[]{"grep", "hello world", "/tmp/f.txt"};
+		     
+		    commands = new String[]{"grep", "hello world", 
+		            "/home/gaellic/Documents/f.txt"};
+		             
+		    Process child = Runtime.getRuntime().exec(commands);
+		     
+		} catch (IOException e) {
+		}*/
+		System.out.println("Compile!");
+
+		
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("sa marche!");
+		
 		String fileName="helloWorld.c";
-		File dir = new File("/ProgramC");
+		File dir = new File("/home/gaellic/workspace/M2Projet/ErrorMeasurement/bin/Compiler");
 		CCompiler test = new CCompiler(fileName,dir);
 		test.Compile();
+		
+		Runtime runtime = Runtime.getRuntime();
+		String[] arg = { "/bin/sh", "-c", "ls / >fichier.txt" };
+		try {
+			final Process process = runtime.exec(arg);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.print("sa marche!");
 		
 
 	}
