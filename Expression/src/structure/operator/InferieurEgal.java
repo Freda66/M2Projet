f31003e3961d00306f4fb3f16189c48481bd3288
Bouboul30 <quentin.faivre30@gmail.com>
2016-01-13 10:55:40 +0100

    Ajout des operateurs souvent utilises.

---
+++ Expression/src/structure/operator/InferieurEgal.java
@@ -0,0 +1,28 @@
+package structure.operator;
+
+import java.util.LinkedList;
+
+import structure.NodeA;
+import structure.SimpleNodeA;
+
+public class InferieurEgal extends Operator {
+
+	@Override
+	public String type() {
+		return "InferieurEgal";
+	}
+	@Override
+	public String toString() {
+		return " <= ";
+	}
+	@Override
+	public SimpleNodeA clone() {
+		return new InferieurEgal();
+	}
+	
+	@Override
+	protected String getSignature() {
+		return Signature.InferieurEgal.toString()  + super.getSignature();
+	}
+
+}
