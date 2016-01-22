5e471c290c28ba59143396da282d13cc83c2888c
PapyPev <pev.arfan@gmail.com>
2016-01-22 13:30:43 +0100

    Prepare Final folder

---
+++ final/src/struct/comparator/Superieur.java
@@ -0,0 +1,39 @@
+package structure.comparator;
+
+import java.util.LinkedList;
+
+import structure.NodeA;
+import structure.SimpleNodeA;
+import structure.operator.Operator;
+
+public class Superieur extends Operator {
+
+	@Override
+	public String type() {
+		return "Superieur";
+	}
+	@Override
+	public String toString() {
+		return " > ";
+	}
+	@Override
+	public SimpleNodeA clone() {
+		return new Superieur();
+	}
+	
+	// Evaluation pour 2 terminaux
+	@Override
+	public void Eval(SimpleNodeA term1, SimpleNodeA term2) {
+		float[] retour= new float[2];
+		retour[0]=0;
+		retour[1]=0;
+	}
+	
+	// Decouverte des arbres equivalents
+	@Override
+	public LinkedList<NodeA> NESOE (){
+		LinkedList<NodeA> l = new LinkedList<NodeA>();
+		return l;
+	}
+
+}
