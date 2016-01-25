ab166b1a22a0663a7916e0db589dfc8eb5255b1e
PapyPev <pev.arfan@gmail.com>
2016-01-25 13:29:42 +0100

    Rm final merge part

--- final/src/ermes/TestBrowseTreeC.java
+++
@@ -1,8 +0,0 @@
-import Tree.BrowseTreeC;
-
-public class TestBrowseTreeC {
-	public static void main(String[] args) {
-    	// Créer un objet dump
-		new BrowseTreeC("myProg.c","programmeC");
-    }
-}
